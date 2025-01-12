e0660e7f7c28edde84e24c105b9f067228f4639d
obsideen <97191699+Pritthish20@users.noreply.github.com>
2025-01-12 19:31:18 +0530

    minor change

--- client/src/pages/auth/Signup.jsx
+++ client/src/pages/auth/Signup.jsx
@@ -24,7 +24,7 @@ const Signup = () => {
 
     await fetchData(form);
     toast.success("Signup successful")
-    navigate("/login");
+    navigate("/");
   };
 
   const isFormValid = form.name && form.password && form.phone;
